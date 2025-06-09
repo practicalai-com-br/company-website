e81ef0e3b9c1583071ea5e49e20e274253c8adef
mauricio.usatai@gmail.com <mauricio.usatai@gmail.com>
2025-06-09 14:45:28 -0300

    Fix vite build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/company-website/',
   build: {
     outDir: './build',
     emptyOutDir: true,
